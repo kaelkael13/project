279d3b581e48ed53f71491b181332892228473e2
kaelkael13 <creativeee99@outlook.com>
2022-06-29 05:13:37 +0300

    popup cart

--- lesson8/shop/addToCart.js
+++ lesson8/shop/addToCart.js
@@ -2,15 +2,39 @@
 
 let productsData = [];
 let productsCards = document.querySelectorAll('.items_1');
+let itemsCounter = document.getElementById('itemsCounter');
+let cart = document.querySelector('.popupCart');
+
+// productsCards.forEach(function (productsCard) {
+let nameItem;
+let desc;
+let price;
+
+// productsData.push({
+//     name,
+//     desc,
+//     price
+// });
+// });
 
 productsCards.forEach(function (productsCard) {
-    let name = productsCard.querySelector('.items_1__header').innerText;
-    let desc = productsCard.querySelector('.items_1__text').innerText;
-    let price = productsCard.querySelector('.itemPrice').innerText;
-
-    productsData.push({
-        name,
-        desc,
-        price
-    });
+    productsCard.querySelector('button').addEventListener('click', function (event) {
+        ++itemsCounter.innerText;
+        let currentItem = event.target.parentNode.parentNode.parentNode;
+        nameItem = currentItem.querySelector('.items_1__header').innerText;
+        desc = currentItem.querySelector('.items_1__text').innerText;
+        price = currentItem.querySelector('.itemPrice').innerText;
+        cart.insertAdjacentHTML('beforeend', addItemList())
+    })
 });
+
+
+function addItemList() {
+    return `
+    <p>${nameItem}</p>
+    <p>${desc}</p>
+    <p>${price}</p>
+    <p>total</p>
+    `
+}
+
